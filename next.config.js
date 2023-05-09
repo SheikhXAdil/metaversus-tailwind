75084ea0444567bf19c41332fda237ce0a6070dd
Adil Nadeem <adilnadeem709@gmail.com>
2023-05-10 00:12:17 +0500

    configured next-config for static export

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  output: 'export',
+  images: {
+    loader: 'custom',
+    loaderFile: './app/image.js',
+  },
 };
 
 module.exports = nextConfig;
